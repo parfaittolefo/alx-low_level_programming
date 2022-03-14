4dfc44d291e0cdaedb6e1724e80ee895b06ca524
parfaittolefo <parfaittolefo23@gmail.com>
2022-03-14 02:52:04 +0100

    Programming C, beging with variables, if, else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,37 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
-        *In main function, we gerarate a rendom number n
-
-        *
-
-        *after that, we verify if it is negative, zero or positive
-
-    */
-
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-    int n;
+	int n;
 
-
-    srand(time(0));
-    n = rand() - RAND_MAX /2;
-
-    if (n < 0)
-    {
-        printf("%d is negative\n",n);
-    }
-
-    else if(n>0)
-    {
-        printf("%d is positive\n",n);
-    }
-
-    else
-        printf("%d is zero\n",n);
-
-    return(0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
