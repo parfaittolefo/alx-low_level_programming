dd1d6ca41b5ced08d37ad3742ed9801190447713
parfaittolefo <parfaittolefo23@gmail.com>
2022-03-14 09:34:06 +0100

    Programming C, beging with variables, if, else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+
 /**
-* We generate a random number
-* we converte it in string
-* get last digit
-*verify it wiyh diffferent condition
-*/
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
     int n, last;
 
     srand(time(0));
-    n = rand()-RAND_MAX / 2;
-    last = n%10;
+
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
 
     if(last > 5)
     {
 
-            printf("Last digit of %d is %d and is greater than 5", n,last);
+            printf("Last digit of %d is %d and is greater than 5",n,last);
 
     }
 
@@ -34,7 +36,7 @@ int main(void)
     else
     {
 
-            printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+            printf("Last digit of %d is %d and is less than 6 and not 0",n, last);
 
     }
 
